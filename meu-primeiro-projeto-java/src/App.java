621e7d14d435fe32da0779d888c02e322045ac17
thiaguinho-dev <thiagolobato2222@gmail.com>
2024-06-18 19:43:44 -0300

    meu-primeiro-projeto-java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("boas vidas turma dio!");
+        System.out.println("BOAS VINDAS TURMA DIO!");
     }
 }
